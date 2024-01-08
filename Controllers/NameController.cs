97ce5183bd2c1369c1c2bc2abd0f46b4b33feb25
Zahid Afsar <zahidtempz@icloud.com>
2024-01-08 11:28:50 -0800

    3rd commit

--- Controllers/NameController.cs
+++ Controllers/NameController.cs
@@ -1,6 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace AfsarZSayHelloEndPoint.Controllers;
 
-    public class NameController
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : ControllerBase
     {
-        
+        public List<string> greeting = new();
+
+        [HttpPost]
+        [Route("NameReturn/{user}")]
+
+        public List<string> NameReturn(string user)
+        {
+            greeting.Add($"Hello, {user}");
+            return greeting;
+        }
+    
     }
+    
